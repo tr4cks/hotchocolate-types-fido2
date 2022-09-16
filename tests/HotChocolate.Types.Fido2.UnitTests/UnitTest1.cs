25fa5a8baecf0cfd538b086c2a22160e110daf2c
odilon <genot.clement@gmail.com>
2022-09-16 15:21:55 +0200

    dotnet format

--- tests/HotChocolate.Types.Fido2.UnitTests/UnitTest1.cs
+++ tests/HotChocolate.Types.Fido2.UnitTests/UnitTest1.cs
@@ -55,7 +55,7 @@ public class UnitTest1
                 builder.AddMutationType<MutationType>();
             });
         var result = await executor.ExecuteAsync(Query);
-        
+
         Assert.Null(result.Errors);
     }
 }
