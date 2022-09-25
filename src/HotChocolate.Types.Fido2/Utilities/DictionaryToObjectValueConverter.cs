c6c595711b4e8c9e3cdf7139322a01e955aa2334
odilon <genot.clement@gmail.com>
2022-09-25 19:45:36 +0200

    implement class to convert dictionary to ObjectValueConverter

---
+++ src/HotChocolate.Types.Fido2/Utilities/DictionaryToObjectValueConverter.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics;
+using HotChocolate.Language;
+using HotChocolate.Utilities;
+
+namespace HotChocolate.Types.Fido2.Utilities;
+
+internal class ConverterContext
+{
+    public IValueNode? ValueNode { get; set; }
+}
+
+internal class DictionaryToObjectValueConverter : DictionaryVisitor<ConverterContext>
+{
+    public ObjectValueNode Convert(IReadOnlyDictionary<string, object?> dictionary)
+    {
+        ConverterContext context = new();
+        VisitObject(dictionary, context);
+        var result = context.ValueNode as ObjectValueNode;
+        Debug.Assert(result != null, nameof(result) + " != null");
+        return result;
+    }
+
+    protected override void VisitObject(IReadOnlyDictionary<string, object?> dictionary, ConverterContext context)
+    {
+        List<ObjectFieldNode> fieldNodes = new();
+        foreach (var field in dictionary)
+        {
+            ConverterContext valueContext = new();
+            VisitField(field, valueContext);
+            Debug.Assert(valueContext.ValueNode != null, "valueContext.ValueNode != null");
+            fieldNodes.Add(new(field.Key, valueContext.ValueNode));
+        }
+        context.ValueNode = new ObjectValueNode(fieldNodes);
+    }
+
+    protected override void VisitList(IReadOnlyList<object> list, ConverterContext context)
+    {
+        List<IValueNode> valueNodes = new();
+        foreach (var value in list)
+        {
+            ConverterContext valueContext = new();
+            Visit(value, valueContext);
+            Debug.Assert(valueContext.ValueNode != null, "valueContext.ValueNode != null");
+            valueNodes.Add(valueContext.ValueNode);
+        }
+        context.ValueNode = new ListValueNode(valueNodes);
+    }
+
+    protected override void VisitValue(object value, ConverterContext context)
+    {
+        context.ValueNode = value switch
+        {
+            null => NullValueNode.Default,
+            string s => new StringValueNode(s),
+            short s => new IntValueNode(s),
+            int i => new IntValueNode(i),
+            long l => new IntValueNode(l),
+            float f => new FloatValueNode(f),
+            double d => new FloatValueNode(d),
+            decimal d => new FloatValueNode(d),
+            bool b => new BooleanValueNode(b),
+            sbyte s => new IntValueNode(s),
+            byte b => new IntValueNode(b),
+            // todo: move this string inside resource file
+            // Unable to convert type from `{0}` to `{1}`
+            _ => throw new NotSupportedException()
+        };
+    }
+}
---
+++ tests/HotChocolate.Types.Fido2.UnitTests/UtilitiesTests.cs
@@ -0,0 +1,40 @@
+using HotChocolate.Types.Fido2.Utilities;
+using HotChocolate.Utilities;
+
+namespace HotChocolate.Types.Fido2.UnitTests;
+
+public class UtilitiesTests
+{
+    private readonly ObjectValueToDictionaryConverter _objectValueToDictionaryConverter = new();
+
+    [Fact]
+    public void DictionaryToObjectValueConverterTest()
+    {
+        DictionaryToObjectValueConverter converter = new();
+        Dictionary<string, object?> originalDictionary = new()
+        {
+            {
+                "object", new Dictionary<string, object?>
+                {
+                    {"nullValue", null},
+                    {"listOfIntegers", new List<object> {1, 2, 3, 4, 5}},
+                    {
+                        "listOfObjects", new List<object>
+                        {
+                            new Dictionary<string, object?>
+                            {
+                                {"int", 42},
+                                {"bool", true}
+                            }
+                        }
+                    }
+                }
+            },
+            {"string", "Hello World!"}
+        };
+        var result = converter.Convert(originalDictionary);
+
+        var dictionaryForTesting = _objectValueToDictionaryConverter.Convert(result);
+        Assert.Equal(originalDictionary, dictionaryForTesting);
+    }
+}
