7130453737329089cfb44b47cfaa623651e16a29
odilon <genot.clement@gmail.com>
2022-10-03 18:37:59 +0200

    Sort declared types

--- src/HotChocolate.Types.Fido2/Extensions/RequestExecutorBuilderExtensions.cs
+++ src/HotChocolate.Types.Fido2/Extensions/RequestExecutorBuilderExtensions.cs
@@ -10,24 +10,30 @@ public static class RequestExecutorBuilderExtensions
 {
     public static IRequestExecutorBuilder AddFido2(this IRequestExecutorBuilder @this) =>
         @this
-            // todo: sort
+            // Register IFido2 service
             .RegisterService<IFido2>()
+
+            // Fido2 input object types
+            .AddType<PublicKeyCredentialAssertionInputType>()
+            .AddType<PublicKeyCredentialAttestationInputType>()
+
+            // Fido2 object types
+            .AddType<AssertionVerificationResultType>()
+            .AddType<AttestationConveyancePreferenceType>()
+            .AddType<AttestationVerificationSuccessType>()
             .AddType<PublicKeyCredentialCreationOptionsType>()
             .AddType<PublicKeyCredentialRequestOptionsType>()
-            .AddType<PublicKeyCredentialAttestationInputType>()
-            .AddType<AttestationVerificationSuccessType>()
-            .AddType<AssertionVerificationResultType>()
-            .AddType<PublicKeyCredentialAssertionInputType>()
-            // todo: sort and add descriptions (auto?)
+
+            // Fido2 enum types
+            // todo: Add descriptions to enum strings (auto?)
             .AddType<AttestationConveyancePreferenceType>()
-            .AddType(new EnumMemberType<AttestationConveyancePreference>(WellKnownScalarTypes
-                .AttestationConveyancePreferenceStringEnum))
-            .AddType<PublicKeyCredentialTypeType>()
-            .AddType(new EnumMemberType<PublicKeyCredentialType>(WellKnownScalarTypes.PublicKeyCredentialTypeStringEnum))
+            .AddType(new EnumMemberType<AttestationConveyancePreference>(WellKnownScalarTypes.AttestationConveyancePreferenceStringEnum))
             .AddType<AuthenticatorAttachmentType>()
             .AddType(new EnumMemberType<AuthenticatorAttachment>(WellKnownScalarTypes.AuthenticatorAttachmentStringEnum))
-            .AddType<UserVerificationRequirementType>()
-            .AddType(new EnumMemberType<UserVerificationRequirement>(WellKnownScalarTypes.UserVerificationRequirementStringEnum))
             .AddType<AuthenticatorTransportType>()
-            .AddType(new EnumMemberType<AuthenticatorTransport>(WellKnownScalarTypes.AuthenticatorTransportStringEnum));
+            .AddType(new EnumMemberType<AuthenticatorTransport>(WellKnownScalarTypes.AuthenticatorTransportStringEnum))
+            .AddType<PublicKeyCredentialTypeType>()
+            .AddType(new EnumMemberType<PublicKeyCredentialType>(WellKnownScalarTypes.PublicKeyCredentialTypeStringEnum))
+            .AddType<UserVerificationRequirementType>()
+            .AddType(new EnumMemberType<UserVerificationRequirement>(WellKnownScalarTypes.UserVerificationRequirementStringEnum));
 }
